3819092ffbd35ac5961b5f50ce40b160dba7e979
Allen Huang <ligerx066@gmail.com>
2020-10-15 16:04:00 -0700

    finished js loops practice

---
+++ javascript-loops-practice/filter-out-nulls.js
@@ -0,0 +1,13 @@
+/* exported filterOutNulls */
+
+function filterOutNulls(values) {
+  var noNull = [];
+
+  for (var i = 0; i < values.length; i++) {
+    if (values[i] !== null) {
+      noNull.push(values[i]);
+    }
+  }
+
+  return noNull;
+}
---
+++ javascript-loops-practice/includes-seven.js
@@ -0,0 +1,10 @@
+/* exported includesSeven */
+
+function includesSeven(values) {
+  for (var i = 0; i < values.length; i++) {
+    if (values[i] === 7) {
+      return true;
+    }
+  }
+  return false;
+}
