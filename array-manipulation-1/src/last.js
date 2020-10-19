f25f23974ee0483ac8d10e75c82a39ef8b8aa562
Allen Huang <ligerx066@gmail.com>
2020-10-19 13:22:20 -0700

    removed if statement in last.js

--- array-manipulation-1/src/last.js
+++ array-manipulation-1/src/last.js
@@ -1,8 +1,5 @@
 /* eslint-disable no-unused-vars */
 
 function last(array) {
-  if (array.length > 0) {
-    return array[array.length - 1];
-  }
-  return undefined;
+  return array[array.length - 1];
 }
