02ca5f9ae3531d692ddc1ee78a4f229c4673a6aa
Allen Huang <ligerx066@gmail.com>
2020-10-18 23:38:04 -0700

    completed sting manipulation 1

--- string-manipulation-1/src/is-upper-cased.js
+++ string-manipulation-1/src/is-upper-cased.js
@@ -1,8 +1,12 @@
 /* eslint-disable no-unused-vars */
 
 function isUpperCased(word) {
+  var lowerCase;
   for (var i = 0; i < word.length; i++) {
-    if (word[i] === word[i].toLowerCase()) return false;
+    lowerCase = word[i].toLowerCase();
+    if (word[i] === lowerCase && lowerCase.charCodeAt(0) >= 97 && lowerCase.charCodeAt(0) <= 122) {
+      return false;
+    }
   }
   return true;
 }
--- string-manipulation-1/src/is-vowel.js
+++ string-manipulation-1/src/is-vowel.js
@@ -1,1 +1,10 @@
 /* eslint-disable no-unused-vars */
+
+function isVowel(character) {
+  var lowerCase = character.toLowerCase();
+  if (lowerCase === 'a' || lowerCase === 'e' || lowerCase === 'i' ||
+  lowerCase === 'o' || lowerCase === 'u') {
+    return true;
+  }
+  return false;
+}
