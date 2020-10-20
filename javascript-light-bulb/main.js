ebc14c6cf2ec8e9decd518f957203efe8a797382
Allen Huang <ligerx066@gmail.com>
2020-10-20 13:28:19 -0700

    added boolean to the function

--- javascript-light-bulb/main.js
+++ javascript-light-bulb/main.js
@@ -1,11 +1,18 @@
 var currentState = document.querySelector('.circle');
 var lightState = currentState.className.split(' ');
+var isLightOn = false;
+
+if (lightState[1] === 'on') {
+  isLightOn = true;
+}
 
 function lightSwitch(event) {
-  if (lightState[1] === 'on') {
+  if (isLightOn) {
     lightState[1] = 'off';
+    isLightOn = false;
   } else {
     lightState[1] = 'on';
+    isLightOn = true;
   }
   currentState.className = lightState[0] + ' ' + lightState[1];
   document.querySelector('body').className = lightState[1];
