fc8a1fbe95c448d4c8cb26561bfb7a78494de6a6
Allen Huang <ligerx066@gmail.com>
2020-10-21 13:04:23 -0700

    finished pick function

--- object-manipulation-2/src/pick.js
+++ object-manipulation-2/src/pick.js
@@ -1,1 +1,16 @@
 /* eslint-disable no-unused-vars */
+
+function pick(source, keys) {
+  var sharedKeys = {};
+  var i;
+
+  for (var objectKey in source) {
+    for (i = 0; i < keys.length; i++) {
+      if (objectKey === keys[i] && source[objectKey] !== undefined) {
+        sharedKeys[objectKey] = source[objectKey];
+        break;
+      }
+    }
+  }
+  return sharedKeys;
+}
