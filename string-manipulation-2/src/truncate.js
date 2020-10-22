d51b47bb0a5bfa6a6d3d5800f95644b8c90a39bf
Allen Huang <ligerx066@gmail.com>
2020-10-22 13:06:14 -0700

    finished truncate function

--- string-manipulation-2/src/truncate.js
+++ string-manipulation-2/src/truncate.js
@@ -1,1 +1,9 @@
 /* eslint-disable no-unused-vars */
+
+function truncate(length, string) {
+  var truncString = '';
+  for (var i = 0; i < length; i++) {
+    truncString += string.charAt(i);
+  }
+  return truncString + '...';
+}
