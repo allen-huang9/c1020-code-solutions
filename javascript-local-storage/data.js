be0bf1c85e5be0f9082d590852104f698a07d813
Allen Huang <ligerx066@gmail.com>
2020-10-23 15:50:19 -0700

    completed js local storage

---
+++ javascript-local-storage/data.js
@@ -0,0 +1,14 @@
+/* exported todos */
+
+var todos = [];
+
+var previousTodosJson = localStorage.getItem('javascript-local-storage');
+
+if (previousTodosJson) {
+  todos = JSON.parse(previousTodosJson);
+}
+
+window.addEventListener('beforeunload', function (e) {
+  var todosJson = JSON.stringify(todos);
+  localStorage.setItem('javascript-local-storage', todosJson);
+});
