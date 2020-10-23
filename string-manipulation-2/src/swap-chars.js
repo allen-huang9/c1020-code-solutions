2465c30d84e274d903554782d839865a74a6c9ba
Allen Huang <ligerx066@gmail.com>
2020-10-22 18:03:15 -0700

    completed string manipulation 2

--- string-manipulation-2/src/swap-chars.js
+++ string-manipulation-2/src/swap-chars.js
@@ -1,1 +1,15 @@
 /* eslint-disable no-unused-vars */
+
+function swapChars(firstIndex, secondIndex, string) {
+  var charArray = string.split('');
+
+  var temp = charArray[firstIndex];
+  charArray[firstIndex] = charArray[secondIndex];
+  charArray[secondIndex] = temp;
+
+  var alteredString = '';
+  for (var i = 0; i < charArray.length; i++) {
+    alteredString += charArray[i];
+  }
+  return alteredString;
+}
