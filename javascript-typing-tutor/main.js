d3ad27c81da708abb8b4db893121dc4331568053
Allen Huang <ligerx066@gmail.com>
2020-10-23 21:11:48 -0700

    working on main.js

---
+++ javascript-typing-tutor/main.js
@@ -0,0 +1,11 @@
+/* eslint no-console: 0 */
+var $sentence = document.querySelectorAll('span');
+var index = 0;
+
+document.addEventListener('keydown', function (e) {
+  if ($sentence[index].textContent !== e.key) {
+    $sentence[index].className += ' incorrect';
+  } else {
+    $sentence[index].className = 'correct';
+  }
+});
