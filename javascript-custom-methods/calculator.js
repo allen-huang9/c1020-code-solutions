e5fa698b269eecbb459eb35f45a2b7d2132d1f62
Allen Huang <ligerx066@gmail.com>
2020-11-02 10:38:39 -0800

    completed js custom methods exercise

---
+++ javascript-custom-methods/calculator.js
@@ -0,0 +1,40 @@
+/* exported calculator */
+
+var calculator = {
+  add: function (x, y) {
+    return x + y;
+  },
+
+  subtract: function (x, y) {
+    return x - y;
+  },
+
+  multiply: function (x, y) {
+    return x * y;
+  },
+
+  divide: function (x, y) {
+    return x / y;
+  },
+
+  square: function (x) {
+    return Math.pow(x, 2);
+  },
+
+  sumAll: function (numbers) {
+    var sum = 0;
+    for (var i = 0; i < numbers.length; i++) {
+      sum += numbers[i];
+    }
+    return sum;
+  },
+
+  getAverage: function (numbers) {
+    var average = 0;
+    for (var i = 0; i < numbers.length; i++) {
+      average += numbers[i];
+    }
+    average = average / numbers.length;
+    return average;
+  }
+};
