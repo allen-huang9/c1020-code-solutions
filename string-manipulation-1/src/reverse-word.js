508e9ffb8f38a30a2dc8efacc0feb182e554743c
Allen Huang <ligerx066@gmail.com>
2020-11-02 18:15:51 -0800

    re-did reverseWord function to only use 1 loop

--- string-manipulation-1/src/reverse-word.js
+++ string-manipulation-1/src/reverse-word.js
@@ -1,23 +1,10 @@
 /* eslint-disable no-unused-vars */
 
 function reverseWord(word) {
-  var temp;
-  var middleIndex = Math.floor(word.length / 2);
-  var array = [];
   var newWord = '';
 
-  for (var k = 0; k < word.length; k++) {
-    array.push(word.charAt(k));
-  }
-
-  for (var i = 0; i < middleIndex; i++) {
-    temp = array[i];
-    array[i] = array[array.length - 1 - i];
-    array[array.length - 1 - i] = temp;
-  }
-
-  for (var j = 0; j < word.length; j++) {
-    newWord += array[j];
+  for (var i = word.length - 1; i >= 0; i--) {
+    newWord += word.charAt(i);
   }
 
   return newWord;
