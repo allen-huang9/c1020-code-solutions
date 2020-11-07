9403e3a4cb259f720293c79dd98ec3e8166571fa
Allen Huang <ligerx066@gmail.com>
2020-11-06 19:02:35 -0800

    created game function

--- working-with-collections/main.js
+++ working-with-collections/main.js
@@ -2,80 +2,82 @@
 /* eslint-disable no-console */
 console.log('Lodash is loaded:', typeof _ !== 'undefined');
 
-var group = [
-  {
-    name: 'player 1',
-    hand: []
-  },
-  {
-    name: 'player 2',
-    hand: []
-  },
-  {
-    name: 'player 3',
-    hand: []
-  },
-  {
-    name: 'player 4',
-    hand: []
-  }
-];
-
-var deck = [];
-var suit = '';
-var rank = '';
-
-for (var i = 0; i < 4; i++) {
-  if (i === 0) {
-    suit = 'diamond';
-  } else if (i === 1) {
-    suit = 'club';
-  } else if (i === 2) {
-    suit = 'heart';
-  } else if (i === 3) {
-    suit = 'spade';
-  }
-
-  for (var j = 1; j < 14; j++) {
-    if (j === 1) {
-      rank = 'Ace';
-    } else if (j === 11) {
-      rank = 'Jack';
-    } else if (j === 12) {
-      rank = 'Queen';
-    } else if (j === 13) {
-      rank = 'King';
-    } else {
-      rank = j;
+function game() {
+  var group = [
+    {
+      name: 'player 1',
+      hand: []
+    },
+    {
+      name: 'player 2',
+      hand: []
+    },
+    {
+      name: 'player 3',
+      hand: []
+    },
+    {
+      name: 'player 4',
+      hand: []
     }
-    var card = {
-      rank: '',
-      suit: ''
-    };
-    card.rank = rank;
-    card.suit = suit;
-    deck.push(card);
+  ];
+
+  var deck = [];
+  var suit = '';
+  var rank = '';
+
+  for (var i = 0; i < 4; i++) {
+    if (i === 0) {
+      suit = 'diamond';
+    } else if (i === 1) {
+      suit = 'club';
+    } else if (i === 2) {
+      suit = 'heart';
+    } else if (i === 3) {
+      suit = 'spade';
+    }
+
+    for (var j = 1; j < 14; j++) {
+      if (j === 1) {
+        rank = 'Ace';
+      } else if (j === 11) {
+        rank = 'Jack';
+      } else if (j === 12) {
+        rank = 'Queen';
+      } else if (j === 13) {
+        rank = 'King';
+      } else {
+        rank = j;
+      }
+      var card = {
+        rank: '',
+        suit: ''
+      };
+      card.rank = rank;
+      card.suit = suit;
+      deck.push(card);
+    }
   }
-}
 
-deck = _.shuffle(deck);
+  deck = _.shuffle(deck);
 
-var handSize = 2;
-var cardIndex = 0;
+  var handSize = 2;
+  var cardIndex = 0;
 
-var playerScore = [];
-for (var p = 0; p < group.length; p++) {
-  for (var h = 0; h < handSize; h++) {
-    group[p].hand.push(deck[cardIndex]);
-    cardIndex++;
+  var playerScore = [];
+  for (var p = 0; p < group.length; p++) {
+    for (var h = 0; h < handSize; h++) {
+      group[p].hand.push(deck[cardIndex]);
+      cardIndex++;
+    }
+    playerScore.push(score(group[p].hand));
+
+    console.log(group[p].name, ' hand is ', group[p].hand);
   }
-  playerScore.push(score(group[p].hand));
 
-  console.log(group[p].name, ' hand is ', group[p].hand);
+  var winner = findWinner(playerScore);
+  console.log('The winner of is ', group[winner].name);
 }
-
-var winner = findWinner(playerScore);
-console.log('The winner of is ', group[winner].name);
 
 function findWinner(scores) {
   var highest = 0;
