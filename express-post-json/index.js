135180399552cc73fa74c9f988660f2e59a44d80
allen-huang9 <ligerx066@gmail.com>
2020-12-01 12:19:11 -0800

    completed express-post-json exercise

---
+++ express-post-json/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+const grades = [];
+let nextId = 1;
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/api/grades', (req, res) => {
+  res.json();
+  console.log(grades);
+});
+
+app.post('/api/grades', (req, res) => {
+  req.body.id = nextId;
+  nextId++;
+  grades.push(req.body);
+  res.status(201).send(req.body);
+});
+
+app.listen(3000, () => {
+  console.log('listening to port 3000');
+});
