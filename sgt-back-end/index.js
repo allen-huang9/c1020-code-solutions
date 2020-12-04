d8254d56034fd417db29ac80219f499d4f946e8f
allen-huang9 <ligerx066@gmail.com>
2020-12-04 15:26:27 -0800

    completed #2, POST /api/grades inserts a new grade into the grades table and returns the created grade

--- sgt-back-end/index.js
+++ sgt-back-end/index.js
@@ -7,6 +7,9 @@ const db = new pg.Pool({
 });
 
 const app = express();
+const jsonMiddleWare = express.json();
+
+app.use(jsonMiddleWare);
 
 app.get('/api/grades', (req, res, next) => {
 
@@ -16,6 +19,40 @@ app.get('/api/grades', (req, res, next) => {
     .then(result => {
       const grades = result.rows;
       res.json(grades);
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({
+        error: 'An unexpected error occurred.'
+      });
+    });
+});
+
+app.post('/api/grades', (req, res, next) => {
+  const sql = `insert into "grades" ("name", "course", "score")
+                  values ($1, $2, $3)`;
+
+  const { name, course, grade } = req.body;
+
+  if (name === undefined || course === undefined || grade === undefined) {
+    res.status(400).send({
+      error: 'The name, course and grade fields are required'
+    });
+    return;
+  }
+
+  const score = parseInt(grade);
+  if (!Number.isInteger(score) || score < 0 || score > 100) {
+    res.status(400).send({
+      error: 'The grade must be a number from 0 to 100'
+    });
+    return;
+  }
+
+  const params = [name, course, score];
+  db.query(sql, params)
+    .then(result => {
+      res.status(201).send(req.body);
     })
     .catch(err => {
       console.error(err);
