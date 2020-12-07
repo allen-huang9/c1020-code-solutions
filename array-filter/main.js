8fe154578214029e495d300283f443687019d3b7
allen-huang9 <ligerx066@gmail.com>
2020-12-07 11:15:48 -0800

    completed array-filter exercise

---
+++ array-filter/main.js
@@ -0,0 +1,31 @@
+/* eslint-disable no-unused-vars, no-console */
+
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+const names = [
+  'Ada',
+  'Hedy',
+  'Jean',
+  'Grace',
+  'Evelyn',
+  'Joan',
+  'Elizabeth',
+  'Janese',
+  'Donna'
+];
+
+const evenNumbers = numbers.filter(number => number % 2 === 0);
+
+console.log('evenNumber: ', evenNumbers);
+
+const overFive = numbers.filter(number => number > 5);
+
+console.log('overFive: ', overFive);
+
+const startWithE = names.filter(name => name[0] === 'E');
+
+console.log('startWithE: ', startWithE);
+
+const haveD = names.filter(name => name.toLowerCase().split('').includes('d'));
+
+console.log('haveD: ', haveD);
