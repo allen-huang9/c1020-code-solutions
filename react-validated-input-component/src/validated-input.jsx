959b0062aaceb71c73665b8f45d5098da2228c4b
allen-huang9 <ligerx066@gmail.com>
2020-12-15 00:13:15 -0800

    completed react validated input component

--- react-validated-input-component/src/validated-input.jsx
+++ react-validated-input-component/src/validated-input.jsx
@@ -5,38 +5,61 @@ class ValidateInput extends React.Component {
     super(props);
     this.state = {
       password: '',
-      isValid: null
+      passwordChanged: false
     };
     this.handlePassword = this.handlePassword.bind(this);
   }
 
   handlePassword(event) {
-    event.preventDefault();
-
-    const { validPassword, reason } = this.isValidPassword(event.target.value);
-
     this.setState({
-      password: event.target.value
+      password: event.target.value,
+      passwordChanged: true
     });
   }
 
-  isValidPassword(event) {
-    const check = {
-      validPassword: false,
-      reason: ''
-    };
-
-    return check;
-  }
-
   render() {
+
+    let inputDisplay;
+    let errorMessage = '';
+    if (!this.state.passwordChanged) {
+      inputDisplay = (
+        <div className="input-container">
+          <input type="password" value={this.state.password} onChange={this.handlePassword}/>
+        </div>
+      );
+    } else if (this.state.passwordChanged && this.state.password.length === 0) {
+      errorMessage = 'A password is required.';
+      inputDisplay = (
+        <div className="input-container">
+          <input type="password" value={this.state.password} onChange={this.handlePassword} />
+          <i className="fas fa-times"></i>
+        </div>
+      );
+    } else if (this.state.passwordChanged && this.state.password.length < 8) {
+      errorMessage = 'Your password is too short.';
+      inputDisplay = (
+        <div className="input-container">
+          <input type="password" value={this.state.password} onChange={this.handlePassword} />
+          <i className="fas fa-times"></i>
+        </div>
+      );
+    } else if (this.state.password.length >= 8) {
+      inputDisplay = (
+        <div className="input-container">
+          <input type="password" value={this.state.password} onChange={this.handlePassword} />
+          <i className="fas fa-check"></i>
+        </div>
+      );
+    }
+
     return (
       <div>
         <form>
           <label>
             Password
-            <input type="text" value={this.state.password} onChange={this.handlePassword}></input>
+            {inputDisplay}
           </label>
+          <p>{errorMessage}</p>
         </form>
       </div>
     );
